0c5c88158ee0fb20800fe43d0dfdfbab4de58847
mschead <marcos@bridge.ufsc.br>
2017-03-23 18:15:47 -0300

    Tirar duvida

--- matriz.h
+++ matriz.h
@@ -2,14 +2,6 @@ class MatrizUtil {
 
 public:
 	MatrizUtil() {
-
-		for (int i = 0; i < 3; i++) {
-			translate[i] = new int[3];
-			for (int j = 0; j < 3; j++) {
-				translate[j] = new int[3];
-			}
-		}
-
 		translate[0][0] = 1;
 		translate[1][0] = 0;
 		translate[2][0] = 0;
@@ -24,16 +16,63 @@ public:
 	}
 
 
-	int** getTranslate(int entryX, int entryY) {
+	void setTranslate(int entryX, int entryY) {
 		translate[2][0] = entryX;
 		translate[2][1] = entryY;
-		return translate;
+		// return translate;
 	}
 
+	int** getTranslate(int entryX, int entryY) {
+		int **t = new int*[3];
+		for (int i = 0; i < 3; i++) {
+			t[i] = new int[3];
+		}
+
+		
+		t[2][0] = entryX;
+		t[2][1] = entryY;
+		return t;
+	}
+
+	void multiplyPointToMatrix(int point[3], int result[3]) {
+		for (int j = 0; j < 3; j++) {
+			for (int i = 0; i < 3; i ++) {
+				result[j] += point[i] * translate[i][j];
+			}
+		}
+	}
+
+	void multiplyMatrices(int a[][3], int b[][3], int result[][3]) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				for(int k = 0; k < 3; k++) {
+					result[i][j] +=  a[i][k] *  b[k][j];
+				}
+			}
+		}
+	}
+
+	void printMatrix3x3(int** a/*[][3]*/) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				printf("%d\t", a[i][j]);
+			}
+			printf("\n");
+		}
+	}
+
+	void printMatrix1x3(int a[3]) {
+		for (int i = 0; i < 3; i++) {
+			printf("%d ", a[i]);
+		}
+	}
+
+
+
 private:	
-	int** translate;
-	int** scale;
-	int** rotate;
+	int translate[3][3];
+	int scale[3][3];
+	// int** rotate;
 
 
 };
--- object/object.h
+++ object/object.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "../coordenada.h"
+#include "../matriz.h"
 
 using namespace std;
 
@@ -37,33 +38,17 @@ public:
 	}
 
 	void translate(int entryX, int entryY) {
-		int transformation[3][3];
-		
-		transformation[0][0] = 1;
-		transformation[1][0] = 0;
-		transformation[2][0] = entryX;
+		MatrizUtil m;
+		m.setTranslate(entryX, entryY);
 
-		transformation[0][1] = 0;
-    	transformation[1][1] = 1;
-		transformation[2][1] = entryY;
-
-		transformation[0][2] = 0;
-		transformation[1][2] = 0;
-		transformation[2][2] = 1;
-
+		//m.printMatrix3x3(m.getTranslate(entryX, entryY));
 
 		for (Coordenada* coordenada : coordenadas) {
-			int point[3];
-			point[0] = coordenada->getX();
-			point[1] = coordenada->getY();
-			point[2] = 1;
-			coordenada->setCoordenada(multiplyPointToMatrix(point, transformation));
+			int result[3] = {0, 0, 0};
+			int point[3] = {coordenada->getX(), coordenada->getY(), 1};
+			m.multiplyPointToMatrix(point, result);
+			coordenada->setCoordenada(result);
 		}
-
-		// for (Coordenada* coordenada : coordenadas) {
-		// 	printf("%d\n", coordenada->getX());
-		// 	printf("%d\n", coordenada->getY());
-		// }
 
 	}
 
@@ -71,108 +56,65 @@ public:
 	void scale(int entryX, int entryY) {
 		Coordenada pontoMedio = this->pontoMedio();
 
-		int translate[3][3];
+		// int translate[3][3];
 		
-		translate[0][0] = 1;
-		translate[1][0] = 0;
-		translate[2][0] = -1 * pontoMedio.getX();
+		// translate[0][0] = 1;
+		// translate[1][0] = 0;
+		// translate[2][0] = -1 * pontoMedio.getX();
 
-		translate[0][1] = 0;
-    	translate[1][1] = 1;
-		translate[2][1] = -1 * pontoMedio.getY();
+		// translate[0][1] = 0;
+  //   	translate[1][1] = 1;
+		// translate[2][1] = -1 * pontoMedio.getY();
 
-		translate[0][2] = 0;
-		translate[1][2] = 0;
-		translate[2][2] = 1;
+		// translate[0][2] = 0;
+		// translate[1][2] = 0;
+		// translate[2][2] = 1;
 
-		int scale[3][3];
+		// int scale[3][3];
 		
-		scale[0][0] = entryX;
-		scale[1][0] = 0;
-		scale[2][0] = 0;
+		// scale[0][0] = entryX;
+		// scale[1][0] = 0;
+		// scale[2][0] = 0;
 
-		scale[0][1] = 0;
-    	scale[1][1] = entryY;
-		scale[2][1] = 0;
+		// scale[0][1] = 0;
+  //   	scale[1][1] = entryY;
+		// scale[2][1] = 0;
 
-		scale[0][2] = 0;
-		scale[1][2] = 0;
-		scale[2][2] = 1;
+		// scale[0][2] = 0;
+		// scale[1][2] = 0;
+		// scale[2][2] = 1;
 
-		int translate2[3][3];
+		// int translate2[3][3];
 		
-		translate2[0][0] = 1;
-		translate2[1][0] = 0;
-		translate2[2][0] = pontoMedio.getX();
+		// translate2[0][0] = 1;
+		// translate2[1][0] = 0;
+		// translate2[2][0] = pontoMedio.getX();
 
-		translate2[0][1] = 0;
-    	translate2[1][1] = 1;
-		translate2[2][1] = pontoMedio.getY();
+		// translate2[0][1] = 0;
+  //   	translate2[1][1] = 1;
+		// translate2[2][1] = pontoMedio.getY();
 
-		translate2[0][2] = 0;
-		translate2[1][2] = 0;
-		translate2[2][2] = 1;
+		// translate2[0][2] = 0;
+		// translate2[1][2] = 0;
+		// translate2[2][2] = 1;
 
-		int result[3][3] = {(0,0,0), (0,0,0), (0,0,0)};
-		int result2[3][3] = {(0,0,0), (0,0,0), (0,0,0)};
+		// int result[3][3] = {(0,0,0), (0,0,0), (0,0,0)};
+		// int result2[3][3] = {(0,0,0), (0,0,0), (0,0,0)};
 
-		multiplyMatrices(translate, scale, result);
-		multiplyMatrices(result, translate2, result2);
+		// multiplyMatrices(translate, scale, result);
+		// multiplyMatrices(result, translate2, result2);
 
-		print_matrix(result2);
+		// print_matrix(result2);
 
-		for (Coordenada* coordenada : coordenadas) {
-			int point[3];
-			point[0] = coordenada->getX();
-			point[1] = coordenada->getY();
-			point[2] = 1;
-			coordenada->setCoordenada(multiplyPointToMatrix(point, result2));
-		}
+		// for (Coordenada* coordenada : coordenadas) {
+		// 	int point[3];
+		// 	point[0] = coordenada->getX();
+		// 	point[1] = coordenada->getY();
+		// 	point[2] = 1;
+		// 	coordenada->setCoordenada(multiplyPointToMatrix(point, result2));
+		// }
 	}
 
-
-	int* multiplyPointToMatrix(int point[3], int transformation[3][3]) {
-		int* result = new int[3];
-		result[0] = 0;
-		result[1] = 0;
-		result[2] = 0;
-
-  	for (int j = 0; j < 3; j++) {
-  		for (int i = 0; i < 3; i ++) {
-    		result[j] += point[i] * transformation[i][j];
-  		}
-  	}
-
-  		return result;
-	}
-
-	void multiplyMatrices(int a[][3], int b[][3], int result[][3])
-	{
-	    int i, j, k;
-	    for(i = 0; i < 3; i++)
-	    {
-	            for(j = 0; j < 3; j++)
-	            {
-	                    for(k = 0; k < 3; k++)
-	                    {
-	                            result[i][j] +=  a[i][k] *  b[k][j];
-	                    }
-	            }
-	    }
-	}
-
-	void print_matrix(int a[][3])
-{
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-            for(j = 0; j < 3; j++)
-            {
-                    printf("%d\t", a[i][j]);
-            }
-            printf("\n");
-    }
- }
 
 protected:
 	char* nome;
